c7e6d1e9a5f88024b3fe6c621198768d36db6d49
Asad King <king.khan648k@gmail.com>
2026-09-18 00:44:14 -0700

    build: Bump versionCode to 2 and versionName to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "sleath.cam.pro"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "2.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
